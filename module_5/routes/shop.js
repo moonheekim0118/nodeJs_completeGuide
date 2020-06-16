cce9396ac55128c144878ea8768f42834481545b
moonee <uuuu118@naver.com>
2020-06-16 12:41:59 +0900

    css styling 추가

--- module_5/routes/shop.js
+++ module_5/routes/shop.js
@@ -2,12 +2,15 @@ const path= require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 
 router.get('/',(req,res,next)=>{
 
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    //res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     // __dirname ==> 현재 파일이 있는 폴더로 가기
     // ../ 해당 폴더의 상위폴더로 가기
 });
