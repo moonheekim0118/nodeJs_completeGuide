7579063ace88f35a90363f34cb8ea77cbe140dd3
moonee <uuuu118@naver.com>
2020-06-16 13:13:19 +0900

    css 파일 분할 후 static 사용

--- module_5/app.js
+++ module_5/app.js
@@ -9,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes); // '/admin' 으로시작하는 path만 보내진다.
 app.use(shopRoutes);
