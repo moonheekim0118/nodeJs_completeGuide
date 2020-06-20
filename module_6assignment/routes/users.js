25d57b1d1e7c4c55e52e7b627c0715b320a90a6f
moonee <uuuu118@naver.com>
2020-06-20 16:33:05 +0900

    add-users 추가 / redirect

--- module_6assignment/routes/users.js
+++ module_6assignment/routes/users.js
@@ -10,15 +10,16 @@ route.get('/users',(req,res,next)=>{
     });
 })
 
-route.post('/users',(req,res,next)=>{
+route.post('/add-users',(req,res,next)=>{ // / 에서 form으로 post들어오면 add-users에서 user_name 저장 후 /users로 리다이렉트
     console.log(req.body.user_name);
     userList.push(req.body.user_name);
-    res.render('users',
+   /* res.render('users',
     {
         path:'/users',
         pageTitle: 'USER LIST',
         userList: userList
-    });
+    });*/
+    res.redirect('/users'); 
 })
 
 module.exports=route;
