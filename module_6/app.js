fe24ce76fb75b2012c9a41af4d75776c3fc2eec1
moonee <uuuu118@naver.com>
2020-06-20 11:29:22 +0900

    ejs 템플릿 엔진

--- module_6/app.js
+++ module_6/app.js
@@ -4,14 +4,10 @@ const express = require('express');
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-
 
 app = express();
 
-app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname:'handlebars'})); //handlebars는 not built in ,pug은 built in, expressHbs() function은 초기화된 뷰 엔진을 return함
-//따라서 해당 엔진을 handlebars 엔진에 지정 
-app.set('view engine', 'handlebars'); //set up
+app.set('view engine', 'ejs'); //set up
 app.set('views', 'views'); //어디에 template이 있는지 알려주는 것 
 
 app.use(bodyParser.urlencoded({extended:false})); //파싱 
