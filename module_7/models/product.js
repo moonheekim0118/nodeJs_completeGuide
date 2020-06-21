2aa5ccea7ff841af35d1787bf550069836a0820f
moonee <uuuu118@naver.com>
2020-06-21 14:41:28 +0900

    refactoring

--- module_7/models/product.js
+++ module_7/models/product.js
@@ -3,6 +3,16 @@
 const fs = require('fs');
 const path = require('path');
 const rootDir = require('../util/path');
+const p = path.join(rootDir,'data','products.json');
+
+const getProductfromFile = cb =>{
+    fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            return cb([]); //해당 콜백함수 인자에 [] 을 넣어서 실행 
+        } //이거 끝나고 다음 코드 실행하지 않기 위해 return
+        cb(JSON.parse(fileContent)); //해당 콜백함수 인자에 stream 데이터가 parsing된 형태를 넣어서 실행
+    });
+}
 
 module.exports= class Product{
     constructor(title){
@@ -10,13 +20,7 @@ module.exports= class Product{
     }
 
     save(){
-       // products.push(this);
-       const p = path.join(rootDir,'data','products.json'); //파일위치 
-       fs.readFile(p, (err, fileContent)=>{
-        let products=[];   
-        if(!err){ //해당 filecontent가 이미 존재한다면  
-            products = JSON.parse(fileContent); //json 파일을 js array로 파싱       
-        }
+       getProductfromFile(products=>{
         products.push(this); // array에 넣기 
         // js -> json 파싱 (stringfiy)
         fs.writeFile(p, JSON.stringify(products), (err)=>{ //다시 파일에 저장 
@@ -27,12 +31,6 @@ module.exports= class Product{
     
     
     static fetchAll(cb){ //콜백함수를 인자로 받아옴 
-        const p = path.join(rootDir,'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            if(err){
-                cb([]); //해당 콜백함수 인자에 [] 을 넣어서 실행 
-            }
-            cb(JSON.parse(fileContent)); //해당 콜백함수 인자에 stream 데이터가 parsing된 형태를 넣어서 실행
-        });
+        getProductfromFile(cb);
     }
 }
