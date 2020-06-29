0c3f8ee6726280a2b78d3a2dce8b16e3c9418472
moonee <uuuu118@naver.com>
2020-06-29 19:21:37 +0900

    user-product association define

--- module_10/app.js
+++ module_10/app.js
@@ -8,21 +8,48 @@ const shopRoute = require('./routes/shop.js');
 const errorsController = require('./controllers/errors');
 const bodyParser = require('body-parser');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 app.set('view engine', 'ejs');
 app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use((req,res,next)=>{ // sequelize 에서 생성한 user를 req에 등록 
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>console.log(err));
+    // req에 저장해놓음으로써, 모든 미들웨어에서 req.user로 접근 가능 
+});
+
 app.use('/admin',adminRoute);
 app.use(shopRoute);
 
 app.use(errorsController.get404page);
 
-sequelize.sync().then(result =>{
-    //console.log(result);
+Product.belongsTo(User,{ constraints: true, onDelete:'CASCADE' }); // User Creates this product
+User.hasMany(Product);
+
+sequelize.sync() // passing {force:true} === override table 
+.then(result =>{
+    return User.findByPk(1);
+})
+.then(user=>{
+    if(!user){
+        return User.create({name:'Moonhee', email:'moonhee118118@gmail.com'});
+    }
+    return Promise.resolve(user);
+})
+.then(user=>{
+    console.log(user);
     app.listen(3000);
-}).catch(err=>{
+})
+.catch(err=>{
     console.log(err);
 }
 ); //모든 model에 있는 table을 생성해준다. ( 이미 존재하면 override 하지 않음 )
--- module_10/controllers/admin.js
+++ module_10/controllers/admin.js
@@ -16,13 +16,12 @@ exports.postAddProduct=(req,res,next)=>{ //상품 추가후 list
     const imageUrl=req.body.imageUrl;
     const price=req.body.price;
     const description=req.body.description;
-    Product.create({
+    req.user.createProduct({
         title: title,
         price: price,
         imageUrl : imageUrl,
-        description : description 
-    })
-    .then(result => {
+        description : description // 저절로 user id 저장 
+    }).then(result => {
         console.log(result);
         res.redirect('/');
     })
@@ -36,9 +35,13 @@ exports.getEditProducts=(req,res,next)=>{
         return res.redirect('/');
     }
     const prodId=req.params.productId;
-    Product.findByPk(prodId)
-    .then(product=>{
-        if(!product) return res.redirect('/');
+    // 현재 req.user와 associated된 Product만 가져오기
+    // 그 Product중에서도 id가 prodId와 같은 것 가져오기 
+    req.user.getProducts({where:{id:prodId}})
+   // Product.findByPk(prodId)
+    .then(products=>{
+        const product= products[0]; 
+        if(!products) return res.redirect('/');
         res.render('admin/edit-product', {
             pageTitle:'Edit product',
             path:'/admin/edit-product',
@@ -56,9 +59,6 @@ exports.postEditProduct=(req,res,next)=>{
     const imageUrl=req.body.imageUrl;
     const price=req.body.price;
     const description=req.body.description;
-    const updatedproduct = new Product(id,title,imageUrl,description,price); //인스턴스 생성, req.body.title(책이름) 생성자 
-    updatedproduct.save(); // products array에 push 
-    res.redirect('/admin/products'); //리다이렉트 
     let changeValue ={
         title:title,
         price:price,
@@ -75,7 +75,7 @@ exports.postEditProduct=(req,res,next)=>{
 
 // /admin/products
 exports.getProducts=(req,res,next)=>{
-    Product.findAll()
+    req.user.getProducts()  // req.user와 associated된 products만 가져오기 
     .then(products =>{
         res.render('admin/products',{ 
             pageTitle:'Admin products',
