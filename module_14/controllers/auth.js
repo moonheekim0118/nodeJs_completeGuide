0806c08e2aec39df283abf1e3bd0ebcf52d0c7b0
moonee <uuuu118@naver.com>
2020-07-13 19:04:12 +0900

    Cookie 저장 및 추출 구현

--- module_14/controllers/auth.js
+++ module_14/controllers/auth.js
@@ -1,15 +1,16 @@
 exports.getLogin=(req,res,next)=>
 {
+    const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
     res.render('auth/login', {
         path:'/login',
         pageTitle:'login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     });
 }
 
 exports.postLogin=(req,res,next)=>
 {
-    req.isLoggedIn=true;
+    res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=10');
     res.redirect('/');
 }
 
