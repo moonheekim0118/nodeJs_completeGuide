b0c6cf8983d3f614410a76bd302e18b0825f0aba
moonee <uuuu118@naver.com>
2020-07-17 23:52:15 +0900

    login / signup validation 구현

--- module_15/routes/auth.js
+++ module_15/routes/auth.js
@@ -1,15 +1,59 @@
 const express= require('express');
 const route = express.Router();
 const authController = require('../controllers/auth');
+const User = require('../models/user');
+const {  body } = require('express-validator/check'); // check function 
 
 route.get('/login', authController.getLogin);
-route.post('/login',authController.postLogin);
+
+route.post('/login',
+[
+    body('email')
+    .isEmail().withMessage('Please Enter a valid email')
+    .custom((value, {req})=>{
+        return User.findOne({email:value})
+        .then(userDoc=>{
+            if(!userDoc){
+                return Promise.reject('Email is wrong');
+            };
+        });
+    })
+    ,
+    body('password')
+    .isLength({min:6, max:12}).withMessage('Password length should be 6~12')
+    .isAlphanumeric().withMessage('only numbers and characters are allowed')
+]
+,authController.postLogin);
 
 route.post('/logout', authController.postLogout);
 
 
 route.get('/signUp',authController.getSignUp);
-route.post('/signUp',authController.postSignUp);
+route.post('/signUp',
+[body('email')
+.isEmail().withMessage('Please Enter a valid email')
+.custom((value, {req})=>{
+    return User.findOne({email:value})
+    .then(userDoc=>{
+        if(userDoc) 
+        { 
+            return Promise.reject('Email exists already. please pick another one')
+        };
+    });
+})
+,
+body('password')
+.isLength({min:6, max:12}).withMessage('Password length should be 6~12')
+.isAlphanumeric().withMessage('only numbers and characters are allowed')
+,
+body('confirmPassword').custom((value, {req})=>{
+    if (value!==req.body.password){
+        throw new Error('Password have to match!');
+    }
+    return true;
+}
+)]
+,authController.postSignUp);
 
 
 route.get('/reset',authController.getReset);
